b75e7ed7736fc3e6122b5d24be383d1d7dde0b7f
Dan <dandole@comcast.net>
2023-05-02 15:43:00 -0400

    Cleanup

--- IPMITools/Ipmi.cs
+++ IPMITools/Ipmi.cs
@@ -1,14 +1,9 @@
 ﻿namespace IPMITools
 {
     using System.Diagnostics;
+
     public class Ipmi
     {
-        private int _percentage = 20;
-        private bool _fanControl = false;
-        private string? _raw;
-        private int _temp;
-        private int _fanSpeed;
-        private int _watts;
         public Ipmi(string programPath, string host, string user, string password)
         {
             ProgramPath = programPath;
@@ -18,15 +13,24 @@
         }
 
         public string ProgramPath { get; set; }
+
         public string Host { get; set; }
+
         public string User { get; set; }
+
         public string Password { get; set; }
-        public int Percentage { get { return _percentage; } }
-        public bool IsControllingFan { get { return _fanControl; } }
-        public string? Raw { get { return _raw; } }
-        public int Temp { get { return _temp; } }
-        public int FanSpeed { get { return _fanSpeed; } }
-        public int Watts { get { return _watts; } }
+
+        public int Percentage { get; private set; } = 20;
+
+        public bool IsControllingFan { get; private set; } = false;
+
+        public string? Raw { get; private set; }
+
+        public int Temp { get; private set; }
+
+        public int FanSpeed { get; private set; }
+
+        public int Watts { get; private set; }
 
         public void LoadSensorOutput()
         {
@@ -35,18 +39,19 @@
 
         public void SetFan(int percentage)
         {
-            if (_fanControl)
+            if (IsControllingFan)
             {
-                _percentage = percentage;
+                Percentage = percentage;
                 byte perc = (byte)percentage;
                 string percHex = "0x" + perc.ToString("X2");
                 Impitool("raw 0X30 0x30 0x02 0xff " + percHex);
             }
         }
+
         public void ControlFan(bool enable)
         {
             Impitool("raw 0X30 0x30 0x01 " + (enable == false ? "0x01" : "0x00"));
-            _fanControl = enable;
+            IsControllingFan = enable;
         }
 
         public void PowerOn()
@@ -58,33 +63,35 @@
         {
             return Impitool("chassis status");
         }
+
         private string Impitool(string arg)
         {
             Process p = new();
             p.StartInfo.UseShellExecute = false;
             p.StartInfo.RedirectStandardOutput = true;
             p.StartInfo.RedirectStandardError = true;
-            p.StartInfo.FileName = this.ProgramPath;
+            p.StartInfo.FileName = ProgramPath;
             p.StartInfo.CreateNoWindow = true;
-            p.StartInfo.Arguments = string.Format("-I lanplus -H {0} -U {1} -P {2} {3}", this.Host, this.User, this.Password, arg);
+            p.StartInfo.Arguments = string.Format("-I lanplus -H {0} -U {1} -P {2} {3}", Host, User, Password, arg);
             p.Start();
             string output = p.StandardOutput.ReadToEnd();
-            //string error = p.StandardError.ReadToEnd();
+
+            // string error = p.StandardError.ReadToEnd();
             p.WaitForExit();
             return output;
         }
 
         private void ProcessSdrList(string data)
         {
-            _raw = data;
+            Raw = data;
             string[] lines = data.Split('\n');
             string? tempLine = lines.Where(x => x.StartsWith("Ambient Temp")).FirstOrDefault();
             string[] fansLines = lines.Where(x => x.StartsWith("FAN ")).ToArray();
             string? wattsLine = lines.Where(x => x.StartsWith("System Level")).FirstOrDefault();
 
-            _temp = int.Parse(tempLine!.Split('|')[1].Replace("degrees C", "").Trim());
-            _fanSpeed = (int)fansLines.Select(x => int.Parse(x.Split('|')[1].Replace("RPM", "").Trim())).Average();
-            _watts = int.Parse(wattsLine!.Split('|')[1].Replace("Watts", "").Trim());
+            Temp = int.Parse(tempLine!.Split('|')[1].Replace("degrees C", string.Empty).Trim());
+            FanSpeed = (int)fansLines.Select(x => int.Parse(x.Split('|')[1].Replace("RPM", string.Empty).Trim())).Average();
+            Watts = int.Parse(wattsLine!.Split('|')[1].Replace("Watts", string.Empty).Trim());
         }
     }
 }
